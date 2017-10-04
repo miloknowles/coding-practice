c15a29076dd157abcbdb77c34743f0b587e4c7a9
Milo Knowles <mknowles@mit.edu>
2017-10-03 20:25:01 -0400

    Add cheat sheet and .gitignore

---
+++ cheat_sheet.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+#include <iostream>
+
+int main(int argc, char const *argv[]) {
+
+	// IO stuff
+
+	// Vectors: compile with -std=c++11
+	std::vector<int> vect = {1, 2, 3, 4, 5, 6, 7, 8};
+	vect.push_back(9);
+	vect.pop_back();
+	vect.back();
+	vect.front();
+
+	for (int i : vect) {
+		std::cout << i << std::endl;
+	}
+
+	for (auto it = begin(vect); it != end(vect); it++) {
+		std::cout << *it << std::endl;
+	}
+
+	return 0;
+}
+
+
